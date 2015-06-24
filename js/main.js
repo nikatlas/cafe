765c6196737e6f6bceb2dcf1b9a6ba0d76a7607d
Nik Atlas <n.atlas@inedu.com>
2015-06-24 10:58:15 +0300

    Fainon orders Done

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 
 var app = angular.module("cafe" , ['ui.router','ngStorage']);
 
+
+var allowedUsers = ["Facebook:868748039884631","Facebook:10207217092088622"];
 var graphUrl = "https://graph.facebook.com/v2.3/";
 
 app.config(function($stateProvider, $urlRouterProvider) {
@@ -21,10 +23,20 @@ app.config(function($stateProvider, $urlRouterProvider) {
       templateUrl: "tmpls/order.html",
       controller: "orderCtrl"
     })
+    .state('orderfood', {
+      url: "/orderfood",
+      templateUrl: "tmpls/orderfood.html",
+      controller: "foodCtrl"
+    })
     .state('fainon', {
       url: "/fainon",
       templateUrl: "tmpls/fainon.html",
-      controller: "orderCtrl"
+      controller: "foodCtrl"
+    })
+    .state('mainfood', {
+      url: "/mainfood",
+      templateUrl: "tmpls/mainfood.html",
+      controller: "foodCtrl"
     });
 });
 
@@ -82,7 +94,85 @@ window.AzureClient = new WindowsAzure.MobileServiceClient(
     "HDGpuYoXFhGxnODiWlLhyoWiYLOuYA65"
 );
 
+app.controller("foodCtrl" , ['$scope' ,  '$rootScope' , '$localStorage' ,function($scope, $rootScope, $localStorage){
+  $scope.foods = [];
+  $scope.food = {
+    comments: ""
+  };
 
+  var getImage = function(i){
+    var lpic = graphUrl + $scope.data[i].object_id + "?access_token=" + $localStorage.fbtoken;
+    $.get(lpic).then(function(result){
+      $scope.$applyAsync(function(){
+        $scope.data[i].lpic = result.images[0].source;
+      });
+    });
+  }
+  $scope.getFainonAll = function(){
+    var fainonId = "662166840552032";
+    var urlToHit = graphUrl + fainonId + "/feed?access_token=" + $localStorage.fbtoken;
+    $.get(urlToHit).then(function(results){
+      $scope.$apply(function(){
+        var r = [];
+        for(var i in results.data){
+          if( results.data[i].type == "photo" ){
+            r.push(results.data[i]);
+          }
+        }
+        $scope.data = r;
+        var today = new Date().getDay();
+        for(var i in $scope.data){
+          if( new Date($scope.data[i].created_time).getDay() == today ){
+            $scope.data[i].simerino = true;
+          }
+          getImage(i);
+        }
+      });
+    });
+  }
+
+  $scope.insert = function () {
+    var table = window.AzureClient.getTable("Food");
+    if( $scope.food.comments.length < 2 )return;
+
+    var obj = {
+      text : $scope.food.comments,
+      user : $rootScope.fbdata.name,
+      userId : $rootScope.user.userId,
+      complete : false
+    };
+    table.insert(obj).done(function(res){
+      alert("Inserted!");
+    });
+  }
+
+  $scope.getAll = function(){
+    var table = window.AzureClient.getTable("Food");
+    table.where({complete : false}).orderByDescending("__updatedAt").read().done(function(res){
+      $scope.$apply(function(){
+        $scope.foods = res;
+      });
+    });
+  }
+  $scope.del = function(item){
+    var table = window.AzureClient.getTable("Food");
+    table.del(item).done(function(){
+      $scope.getAll();
+    });
+  }
+
+
+  $scope.orderComplete = function(){
+    if( $rootScope.user ){
+      if( allowedUsers.indexOf($rootScope.user.userId) > -1 ){
+        window.AzureClient.invokeApi("Helpers/foodOrderConfirm/" + $rootScope.fbdata.name , {body:null , method: "post"}).done(function(){
+          $scope.getAll();
+        });
+      }
+    }
+  }
+
+}]);
 
 app.controller("orderCtrl",['$scope' , '$rootScope' , '$localStorage' , function($scope, $rootScope, $localStorage) {
   $scope.orders = [];
@@ -171,38 +261,6 @@ app.controller("orderCtrl",['$scope' , '$rootScope' , '$localStorage' , function
       });
     });
   }
-  var getImage = function(i){
-    var lpic = graphUrl + $scope.data[i].object_id + "?access_token=" + $localStorage.fbtoken;
-    $.get(lpic).then(function(result){
-      $scope.$applyAsync(function(){
-        $scope.data[i].lpic = result.images[0].source;
-      });
-    });
-  }
-  $scope.getFainonAll = function(){
-    var fainonId = "662166840552032";
-    var urlToHit = graphUrl + fainonId + "/feed?access_token=" + $localStorage.fbtoken;
-    $.get(urlToHit).then(function(results){
-      $scope.$apply(function(){
-        var r = [];
-        for(var i in results.data){
-          if( results.data[i].type == "photo" ){
-            r.push(results.data[i]);
-          }
-        }
-        $scope.data = r;
-        var today = new Date().getDay();
-        for(var i in $scope.data){
-          if( new Date($scope.data[i].created_time).getDay() == today ){
-            $scope.data[i].simerino = true;
-          }
-          getImage(i);
-        }
-      });
-    });
-
-  }
-
   $scope.del = function(item){
     var table = window.AzureClient.getTable("Order");
     table.del(item).done(function(){
@@ -210,7 +268,6 @@ app.controller("orderCtrl",['$scope' , '$rootScope' , '$localStorage' , function
     });
   }
 
-  var allowedUsers = ["Facebook:868748039884631","Facebook:10207217092088622"];
 
   $scope.orderComplete = function(){
     if( $rootScope.user ){
